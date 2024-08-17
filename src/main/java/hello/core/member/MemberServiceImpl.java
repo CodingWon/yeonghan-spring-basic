6f300fc0977c9ca727e6fd498a0e6bd14570650c
kjw <kingcoding2298@gmail.com>
2024-08-17 12:19:26 +0900

    sec2-회원 도메인, 실행과 테스트
    - 이슈 사항 : MemberServiceImpl 에서 MemberRepository 와 MemoryMemberRepositroy를 의존하고 있다.
    
    public class MemberServiceImpl implements  MemberService {
       private final MemberRepository memberRepository = new MemoryMemberRepositroy();
        ...
    }

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,12 +1,17 @@
 package hello.core.member;
 
+/*
+ *  회원 도메인 설계의 문제점
+ *   - 다른 저장소로 변경할 때 OCP 원칙을 잘 준수 할까?
+ *   - DIP를 잘 지키고 있을 까?
+ *   - 의존 관계가 인터페이스 뿐만 아니라 구현 까지 모두 의존하는 문제점이 있음
+ *
+ *   - 현재 추상화에도 의존하고 구체화 에도 의존하고 있다.
+ * */
+
 public class MemberServiceImpl implements  MemberService{
 
-    private final MemberRepository memberRepository;
-
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+    private final MemberRepository memberRepository = new MemoryMemberRepositroy();
 
     @Override
     public void join(Member member) {
